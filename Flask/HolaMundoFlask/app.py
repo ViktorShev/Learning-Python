05e02025d7fb4699722adfa73f34ec158615a053
Viktor Shevtsov <viktross736@hotmail.com>
2021-08-07 17:21:49 +0300

    added error404 handler and error template

--- Flask/HolaMundoFlask/app.py
+++ Flask/HolaMundoFlask/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from flask.helpers import url_for
+from flask.json import jsonify
+from werkzeug.exceptions import abort
 from werkzeug.utils import redirect
 
 
@@ -27,3 +29,21 @@ def mostrar_nombre(nombre):
 @app.route('/redir')
 def redir():
     return redirect(url_for('mostrar_nombre', nombre='Juan'))
+
+@app.route('/salir')
+def salir():
+    return abort(404)
+
+@app.errorhandler(404)
+def pagina404(error):
+    return render_template('error404.html', error=error), 404
+
+#REST = Representational state transfer
+
+@app.route('/api/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_json(nombre):
+    valores = {
+        'nombre': nombre,
+        'metodo_http': request.method
+    }
+    return jsonify(valores)
