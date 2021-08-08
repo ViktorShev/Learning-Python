c81cf7439784a54d137e2cef1254837327c41bfb
Viktor Shevtsov <viktross736@hotmail.com>
2021-08-08 19:00:37 +0300

    keywords in python

---
+++ Profundizando-Python/keywords.py
@@ -0,0 +1,12 @@
+import keyword
+
+
+print('Palabras reservadas (keywords) En Python')
+print(keyword.kwlist)
+
+# Variable (no podemos utilizar keywords para el nombre de una variable)
+#as = 'Hola'
+
+# Funcion (no podemos utilizar keywords para el nombre de una funcion)
+#def is():
+#    pass
