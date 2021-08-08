4dc05e8484e76a36d31fb1d9e9689ce9bc1bff24
Viktor Shevtsov <viktross736@hotmail.com>
2021-08-08 18:26:30 +0300

    decorator changes

--- Profundizando-Python/decoradores.py
+++ Profundizando-Python/decoradores.py
@@ -6,23 +6,31 @@
 # 3. Funcion decorada (c)
 
 def funcion_decorador_a(funcion_a_decorar_b):
-    def funcion_decorada_c():
+    def funcion_decorada_c(*args, **kwargs):
         print('Antes de ejecutar la funcion')
-        funcion_a_decorar_b()
+        resultado = funcion_a_decorar_b(*args, **kwargs)
         print('Despues de ejecutar la funcion')
+        return resultado
     
     return funcion_decorada_c
 
 
+#@funcion_decorador_a
+#def mostrar_mensaje():
+#    print('Hola desde funcion mostrar_mensaje')
+#
+#mostrar_mensaje()
+#
+#@funcion_decorador_a
+#def imprimir():
+#    print('Hola desde funcion imprimir')
+#
+#print()
+#imprimir()
+
 @funcion_decorador_a
-def mostrar_mensaje():
-    print('Hola desde funcion mostrar_mensaje')
+def sumar(a,b):
+    return a + b
 
-mostrar_mensaje()
-
-@funcion_decorador_a
-def imprimir():
-    print('Hola desde funcion imprimir')
-
-print()
-imprimir()
+resultado = sumar(5, 6)
+print(f'Resultado de la suma: {resultado}')
