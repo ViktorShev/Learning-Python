259f13107ed79e0ba45e942c6d35a3e6d01ea934
Viktor Shevtsov <viktross736@hotmail.com>
2021-08-18 20:59:31 +0300

    Fixed function and added second solution

--- contar_islas_numeros.py
+++ contar_islas_numeros.py
@@ -1,13 +1,24 @@
-arr = [1,0,0,0,0,0,1,1,1,1,0,0,1,1,1,0,0,0,1,1,1]
+arr2 = [1,0,0,0,0,0,1,1,1,1,0,0,1,1,1,0,0,0,1,1,1]
 
 def contar(arr):
     islands = 0
-    arr.insert(0, 0)                                   
-    for i in range(len(arr)):
-        if arr[i-1] == 0 and arr[i] == 1:
+    new_arr = arr.insert(0, 0)                                   
+    for i in range(1, len(new_arr)):
+        if new_arr[i-1] == 0 and new_arr[i] == 1:
             islands += 1
 
     return islands
     
+def contar2(arr):
+    islands = 0
+    previousState = 0
+    for i in range(len(arr)):
+        if previousState == 0 and arr[i] == 1:
+          islands += 1
+        previousState =  arr[i]
 
-print(contar(arr))
+    return islands
+
+
+print(contar2(arr2))
+print(contar(arr2))
