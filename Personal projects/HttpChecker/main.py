0042eb17d450790f60e5741b6a38f4346e18ef38
Viktor Shevtsov <viktross736@hotmail.com>
2021-10-29 22:35:35 +0300

    Added func printResInfo and custom help message

--- Personal projects/HttpChecker/main.py
+++ Personal projects/HttpChecker/main.py
@@ -1,21 +1,24 @@
-#!
+#!/usr/bin/python3
 
 # Native modules
+
 import os
 import pathlib
+from customArgParser import CustomArgumentParser
 
 # Non-native modules
+
 import requests
 import argparse
 
 
 # Initializing terminal parser and adding commands
 
-parser = argparse.ArgumentParser(prog='HTTPChecker 1.0', description='Check for working/listening web-servers', add_help=False)
+parser = CustomArgumentParser(prog='HTTPChecker 1.0', description='Check for working/listening web-servers')
 
-parser.add_argument('-f', '--file', help='Specify the path to the file with the subdomains or IP\'s', metavar='\b', required=True)
-parser.add_argument('-o', '--output', help='Save the check results into a file', metavar='\b')
-parser.add_argument('--add-schema', help='Add the specified schema to the start of all hosts in the file', metavar='\b')
+parser.add_argument('-f', '--file', metavar='\b', required=True)
+parser.add_argument('-o', '--output', metavar='\b')
+parser.add_argument('--add-schema', metavar='\b')
 args = parser.parse_args()
 
 
@@ -30,7 +33,7 @@ with open(pathToFile, 'r') as f:
 
 schemas = {
     'https': 'https://',
-    'http': 'https://',
+    'http': 'http://',
     'ftp': 'ftp://'
 }
 
@@ -43,12 +46,23 @@ if args.__dict__['add_schema'] is not None:
 # Checking for HTTP/HTTPS Web-servers
 
 results = []
+
+def printResInfo(res=None, redir=False):
+    if redir:
+        print(f'{host}  ---  code: {res.status_code}  ---  redirects to: ' + res.headers['Location'])
+        results.append(f'{host}  ---  code: {res.status_code}  ---  redirects to: ' + res.headers['Location'])
+    else:
+        print(f'{host}  ---  code: {res.status_code}')
+        results.append(f'{host}  ---  code: {res.status_code}')
+
 for host in hosts:
     try:
         try:
-            req = requests.get(host, headers={'connection': 'close'})
-            print(f'{host}   ---   {req.status_code}')
-            results.append(f'{host}   ---   {req.status_code}')
+            res = requests.head(host, headers={'connection': 'close'})
+            if res.status_code in [301, 302, 303, 307]:
+                printResInfo(res, redir=True)
+            else:
+                printResInfo(res)
         except requests.exceptions.ConnectionError as e:
             print(f'{host}   ---   Doesn\'t exist or couldn\'t establish a connection.')
     except requests.exceptions.MissingSchema as e:
